a6c84b4cdd8c5d65d62f8e26d33e282b83a320f8
Kristoffer <kristoffer.bekkevold@outlook.com>
2025-11-23 21:58:39 +0100

    Add initial implementation for reading and parsing input data

--- 2025/Day01/Program.cs
+++ 2025/Day01/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Read all lines from the input file
+var input = File.ReadAllLines("input.txt");
+
+// Parse the file content - split each line into two numbers
+var leftNumbers = new List<int>();
+var rightNumbers = new List<int>();
+
+foreach (var line in input)
+{
+    var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+    leftNumbers.Add(int.Parse(parts[0]));
+    rightNumbers.Add(int.Parse(parts[1]));
+}
+
+// Print numbers
+Console.WriteLine("Left numbers:");
+foreach (var number in leftNumbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("\nRight numbers:");
+foreach (var number in rightNumbers)
+{
+    Console.WriteLine(number);
+}
